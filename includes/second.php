bc8d2024c3b7215ee1ba6b0cc671a87c00baf90f
karthik <karthik.trn@gmail.com>
2013-03-05 17:46:40 -0500

    lookup updated and linked to homepage
    lookup updated and linked to homepage

--- includes/second.php
+++ includes/second.php
@@ -8,13 +8,39 @@
         }
         
         h2 { color: #FFF; }
+        #eyeimg{
+            padding-top: 2px;
+            padding-bottom: -2px;
+            padding-left: 10px;
+            padding-right: 10px;         
+            width: 20px;
+            height: 20px;
+            border-bottom-right-radius: 10px;
+            border-top-left-radius: 10px;
+        }
+        a{
+            color: rgb(0, 0, 0);
+            text-decoration: none;
+            padding: 10px;
+            background: rgb(255, 255, 255);
+            -webkit-box-shadow: 2px 2px 2px rgb(0, 0, 0);
+            border: 1px solid rgb(109, 104, 104);
+            border-bottom-left-radius: 10px;
+            /*border-bottom-right-radius: 10px;
+            border-top-left-radius: 10px;
+            */
+            border-top-right-radius: 10px;
+        }
+        p{
+            padding: 5px;
+        }
     </style>
 </head>
 <div id="secondary">
     <h2>Hello There!...</h2>
     <p>Welcome to site lookup</p>
-    <p>Help comes here</p>
-    <p>Help comes here</p>
-    <p>Help comes here</p>
-    <p>Help comes here</p>
+    <p><a href="../Home_lookup.php" target="_parent" ><img src="../images/eye.gif" id="eyeimg">Lookup Home page</a></p>
+    <p><a href="../Home_lookup.php" target="_parent" ><img src="../images/eye.gif" id="eyeimg">Lookup Home page</a></p>
+    <p><a href="../Home_lookup.php" target="_parent" ><img src="../images/eye.gif" id="eyeimg">Lookup Home page</a></p>
+    <p><a href="../Home_lookup.php" target="_parent" ><img src="../images/eye.gif" id="eyeimg">Lookup Home page</a></p>
 </div>
