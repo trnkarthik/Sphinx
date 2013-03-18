8a1c0881673e523340911cc069fd00b209edc7c8
karthik <karthik.trn@gmail.com>
2013-03-18 16:03:14 -0400

    Login updated
    Login updated.

--- includes/login_check.php
+++ includes/login_check.php
@@ -8,26 +8,29 @@ require_once("functions.php");
 
 $username=$_POST['login'];
 $password=$_POST['password'];
-$hashed_password=sha1($password);
-echo $username."<br/>".$password;
+$hashed_password=md5($password);
+echo $username."<br/>".$hashed_password;
 
-
-
-$query="select user_id,user_email from users
-        where user_email='{$username}' and hashed_password='{$hashed_password}' ";
+$query="select user_id,user_name from sp_user_login
+        where password='$hashed_password' and  (user_email = '$username' OR user_name ='$username') ";
+        
+        //echo $query;
+        
 $result_set=mysql_query($query);
-
 
 if(mysql_num_rows($result_set)==1)
 {
     $found_user=mysql_fetch_array($result_set);
     $_SESSION[user_id]=$found_user['user_id'];
-    $_SESSION[username]=$found_user['username'];
-    redirect_to("../login_failed.php");
+    $_SESSION[username]=$found_user['user_name'];
+    //echo "<br/>".$_SESSION[user_id]."<br/>".$_SESSION[username];
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    //redirect_to("../login_failed.php");
 }
 else
 {
-    redirect_to("../index.php");    
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    //redirect_to("../index.php");    
 }
 
 
